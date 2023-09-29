0fe8fd3dac1d10ac30324458368e3a68f9debf64
BeroBurny <bero4net@gmail.com>
2023-09-29 10:27:27 +0200

    reproduction

---
+++ app/(sec)/sec/page.tsx
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+export default function Sub() {
+  return (
+    <div>
+      <h1>Bad day?</h1>
+      <br />
+      <Link href="/">Go to Home</Link>
+    </div>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,13 @@
-/** Add your relevant code here for the issue to reproduce */
+import Link from "next/link";
+
 export default function Home() {
-  return null
+  return (
+    <div>
+      <h1>Good day?</h1>
+      <br />
+      <Link href="/sub">Go to Sub</Link>
+      <br />
+      <Link href="/sec">Go to Sec</Link>
+    </div>
+  );
 }
